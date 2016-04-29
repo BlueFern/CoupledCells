77fff346a2bdba37d6b18e60cc1991803b20f87e
pletzera <pletzera@niwa-1007638.niwa.local>
2016-04-29 15:09:17 +1200

    ~ suggest what to do if SUNDIALS was not found

--- CMake/FindSUNDIALS.cmake
+++ CMake/FindSUNDIALS.cmake
@@ -26,7 +26,7 @@ IF(EXISTS ${SUNDIALS_DIR}/include/sundials/sundials_config.h)
 
 ELSE(EXISTS ${SUNDIALS_DIR}/include/sundials/sundials_config.h)
 	SET(SUNDIALS_FOUND NO)
-	message(FATAL_ERROR "Can not find SUNDIALS!")
+	message(FATAL_ERROR "Cannot find SUNDIALS! Set SUNDIAL_DIR to point to the top directory")
 ENDIF(EXISTS ${SUNDIALS_DIR}/include/sundials/sundials_config.h)
 
 include(FindPackageHandleStandardArgs)
