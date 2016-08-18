c1eb57636586e32b4266664cb528fe02793ceabd
Stewart Dowding <sdo1458@gmail.com>
2016-08-18 16:19:19 +1200

    Modify coupling coefficients.

--- src/compute.cpp
+++ src/compute.cpp
@@ -29,15 +29,15 @@ void set_coupling_parms(int CASE, conductance* cpl_cef)
 {
 	// These values are honestly just trial and error guesses at what makes nice waves...
 	// Really need to have a think about what they should be.
-	cpl_cef->ec_diffusion[0] = 1;
-	cpl_cef->ec_diffusion[1] = 1;
-	cpl_cef->ec_diffusion[2] = 1;
-	cpl_cef->ec_diffusion[3] = 1;
+	cpl_cef->ec_diffusion[0] = 0.15;
+	cpl_cef->ec_diffusion[1] = 0.15;
+	cpl_cef->ec_diffusion[2] = 0.35;
+	cpl_cef->ec_diffusion[3] = 0.35;
 
-	cpl_cef->smc_diffusion[0] = 1;
-	cpl_cef->smc_diffusion[1] = 1;
-	cpl_cef->smc_diffusion[2] = 1;
-	cpl_cef->smc_diffusion[3] = 1;
+	cpl_cef->smc_diffusion[0] = 0.35;
+	cpl_cef->smc_diffusion[1] = 0.35;
+	cpl_cef->smc_diffusion[2] = 0.15;
+	cpl_cef->smc_diffusion[3] = 0.15;
 
 
 	if(CASE == 1)
